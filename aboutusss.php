f8f0b550a6801e7ebd168f389937369701072e85
vijayalaxm <79500954+vijayalaxm@users.noreply.github.com>
2021-09-24 08:35:56 +0530

    Add files via upload

---
+++ aboutusss.php
@@ -0,0 +1,47 @@
+<html>
+<head> 
+<title>traing center</title>
+<link rel="stylesheet" href="sssss.css">
+</head>
+<body>
+    <div class="menu-bar">
+        <ul>
+            <li>home</li>
+            <li><a href="#">home</a></li>
+            <li><a href="aboutus.html">about us</a></li>
+            <li><a href="#">testmonials</a></li>
+            <li><a href="#">gallery</a></li>
+            <li><a href="contactus.php">Contact us</a></li>
+            <li><a href="login.html">View</a></li>
+        </ul>
+        </div>
+        <div class="section">
+		<div class="container">
+			<div class="content-section">
+				<div class="title">
+					<h1>About Us</h1>
+				</div>
+				<div class="content">
+					<h3>Lorem ipsum dolor sit amet, consectetur adipisicing</h3>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat.</p>
+					<div class="button">
+						<a href="">Read More</a>
+					</div>
+				</div>
+				<div class="social">
+					<a href=""><i class="fab fa-facebook-f"></i></a>
+					<a href=""><i class="fab fa-twitter"></i></a>
+					<a href=""><i class="fab fa-instagram"></i></a>
+				</div>
+			</div>
+			<div class="image-section">
+				<img src="image1.jpg">
+			</div>
+		</div>
+	</div>
+</body>
+</html>
+        
